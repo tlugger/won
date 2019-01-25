c4ec9d0ad226978f2d495587c8e60b95e484f952
Tyler Lugger <notnottyler@gmail.com>
2019-01-25 10:02:41 -0700

    Add cli library

--- won.go
+++ won.go
@@ -1,57 +1,79 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "os"
-  "strings"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/urfave/cli"
 )
 
+// Weather unpacks the DarkSky data
 type Weather struct {
-  Current Currently `json:"currently"`
+	Current Currently `json:"currently"`
 }
 
+// Currently pulls summary and temperature from currently
 type Currently struct {
-  Summary string `json:"summary"`
-  Temperature float64 `json:"temperature"`
+	Summary     string  `json:"summary"`
+	Temperature float64 `json:"temperature"`
 }
 
-func getRequest(url string) (*http.Response, error){
-  resp, err := http.Get(url)
-  return resp, err
+func getRequest(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	return resp, err
 }
 
-func getWeather(body []byte) (*Weather, error){
-  var w = new(Weather)
-  err := json.Unmarshal(body, &w)
+func getWeather(body []byte) (*Weather, error) {
+	var w = new(Weather)
+	err := json.Unmarshal(body, &w)
 
-  return w, err
+	return w, err
+}
+
+func getConditions() (string, float64) {
+	apikey := os.Getenv("WONAPIKEY")
+	latLong := os.Getenv("WONLATLONG")
+
+	s := []string{"https://api.darksky.net/forecast", apikey, latLong}
+	url := strings.Join(s, "/")
+
+	resp, err := getRequest(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w, err := getWeather([]byte(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return w.Current.Summary, w.Current.Temperature
 }
 
 func main() {
-  apikey := os.Getenv("WONAPIKEY")
-  latLong := os.Getenv("WONLATLONG")
+	current, temperature := getConditions()
 
-  s := []string{"https://api.darksky.net/forecast", apikey, latLong}
-  url := strings.Join(s, "/")
+	app := cli.NewApp()
+	app.Name = "won"
+	app.Usage = "Weather or not (here it comes)"
+	app.Action = func(c *cli.Context) error {
+		fmt.Printf("Current condition: %s\n", current)
+		fmt.Printf("Current temperature: %.2f°F\n", temperature)
+		return nil
+	}
 
-  resp, err := getRequest(url)
-  if err != nil{
-    panic(err.Error())
-  }
-
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    panic(err.Error())
-  }
-
-  w, err := getWeather([]byte(body))
-  if err != nil {
-    panic(err.Error())
-  }
-
-  fmt.Printf("Current condition: %s\n", w.Current.Summary)
-  fmt.Printf("Current temperature: %.2f°F\n", w.Current.Temperature)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
